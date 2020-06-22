05c3db6ea94749aa24797054e90cf4c5b3521298
hesenrun <hesenrun919359170@outlook.com>
2020-06-22 11:19:10 +0800

    小更，准备弃坑

--- app/src/main/java/com/bqmz001/codiv19panel/ChartActivity.java
+++ app/src/main/java/com/bqmz001/codiv19panel/ChartActivity.java
@@ -7,6 +7,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.bqmz001.codiv19panel.data.TrendDetail;
@@ -28,8 +33,17 @@ public class ChartActivity extends AppCompatActivity {
     ActionBar actionBar;
     String json;
     LineChartView lineChartView;
+    Spinner s_start, s_end;
+    Button button;
 
     List<TrendDetail> trends;
+
+    List<String> dates;
+
+    List<TrendDetail> chooseTrends;
+
+    int StartPosition = 0;
+    int EndPosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +53,82 @@ public class ChartActivity extends AppCompatActivity {
         json = intent.getStringExtra("trend");
         trends = new Gson().fromJson(json, new TypeToken<List<TrendDetail>>() {
         }.getType());
+
+        dates = new ArrayList<>();
+        for (TrendDetail trend : trends) {
+            dates.add(trend.getDay());
+        }
+
+        StartPosition = trends.size() - 30;
+        EndPosition = trends.size() - 1;
+
+
+        chooseTrends = new ArrayList<>();
+        for (int i = trends.size() - 30; i < trends.size(); i++) {
+            chooseTrends.add(trends.get(i));
+        }
+
+
+        s_start = findViewById(R.id.spinner_startTime);
+        s_end = findViewById(R.id.spinner_endTime);
+
+        ArrayAdapter adapter_s = new ArrayAdapter(ChartActivity.this, R.layout.support_simple_spinner_dropdown_item, dates);
+        adapter_s.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+        s_start.setAdapter(adapter_s);
+        s_start.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                StartPosition = position;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        s_end.setAdapter(adapter_s);
+        s_end.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                EndPosition = position;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        s_start.setSelection(StartPosition);
+        s_end.setSelection(EndPosition);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button.setClickable(false);
+                if (StartPosition < EndPosition) {
+                    chooseTrends.clear();
+                    for (int i = StartPosition; i <= EndPosition; i++) {
+                        chooseTrends.add(trends.get(i));
+                    }
+                    initChart();
+                } else {
+                    Toast.makeText(ChartActivity.this,"开始时间须小于结束时间，请重新选择",Toast.LENGTH_SHORT).show();
+                    StartPosition = trends.size() - 30;
+                    EndPosition = trends.size() - 1;
+                    s_start.setSelection(StartPosition);
+                    s_end.setSelection(EndPosition);
+                    chooseTrends.clear();
+                    for (int i = StartPosition; i <= EndPosition; i++) {
+                        chooseTrends.add(trends.get(i));
+                    }
+                    initChart();
+                }
+                button.setClickable(true);
+            }
+        });
 
         lineChartView = findViewById(R.id.lineChart);
         actionBar = getSupportActionBar();
@@ -60,12 +150,12 @@ public class ChartActivity extends AppCompatActivity {
 
         List<AxisValue> axisXValues = new ArrayList<>();
 
-        for (int i = 0; i < trends.size(); i++) {
-            axisXValues.add(new AxisValue(i).setLabel(trends.get(i).getDay()));
-            sure_cnts.add(new PointValue(i, trends.get(i).getSure_cnt()));
-            die_cnts.add(new PointValue(i, trends.get(i).getDie_cnt()));
-            cure_cnts.add(new PointValue(i, trends.get(i).getCure_cnt()));
-            doubt_cnts.add(new PointValue(i, trends.get(i).getDoubt_cnt()));
+        for (int i = 0; i < chooseTrends.size(); i++) {
+            axisXValues.add(new AxisValue(i).setLabel(chooseTrends.get(i).getDay()));
+            sure_cnts.add(new PointValue(i, chooseTrends.get(i).getSure_cnt()));
+            die_cnts.add(new PointValue(i, chooseTrends.get(i).getDie_cnt()));
+            cure_cnts.add(new PointValue(i, chooseTrends.get(i).getCure_cnt()));
+            doubt_cnts.add(new PointValue(i, chooseTrends.get(i).getDoubt_cnt()));
         }
 
         Axis axisX = new Axis();
@@ -113,16 +203,16 @@ public class ChartActivity extends AppCompatActivity {
         public void onValueSelected(int lineIndex, int pointIndex, PointValue value) {
             switch (lineIndex) {
                 case 0:
-                    Toast.makeText(getApplicationContext(), trends.get(pointIndex).getDay()+" 累计确诊人数:"+trends.get(pointIndex).getSure_cnt(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), chooseTrends.get(pointIndex).getDay() + " 累计确诊人数:" + chooseTrends.get(pointIndex).getSure_cnt(), Toast.LENGTH_SHORT).show();
                     break;
                 case 1:
-                    Toast.makeText(getApplicationContext(), trends.get(pointIndex).getDay()+" 累计死亡人数:"+trends.get(pointIndex).getDie_cnt(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), chooseTrends.get(pointIndex).getDay() + " 累计死亡人数:" + chooseTrends.get(pointIndex).getDie_cnt(), Toast.LENGTH_SHORT).show();
                     break;
                 case 2:
-                    Toast.makeText(getApplicationContext(), trends.get(pointIndex).getDay()+" 累计治愈人数:"+trends.get(pointIndex).getCure_cnt(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), chooseTrends.get(pointIndex).getDay() + " 累计治愈人数:" + chooseTrends.get(pointIndex).getCure_cnt(), Toast.LENGTH_SHORT).show();
                     break;
                 case 3:
-                    Toast.makeText(getApplicationContext(), trends.get(pointIndex).getDay()+" 当日疑似人数:"+trends.get(pointIndex).getDoubt_cnt(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), chooseTrends.get(pointIndex).getDay() + " 当日疑似人数:" + chooseTrends.get(pointIndex).getDoubt_cnt(), Toast.LENGTH_SHORT).show();
                     break;
             }
         }
